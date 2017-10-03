009b8a79fa626ac1b511566a8dedd86105eb833b
gengjie02 <gengjie02@meituan.com>
2017-10-03 14:31:33 +0800

    添加通用站点搜索

--- crawler/settings.py
+++ crawler/settings.py
@@ -14,7 +14,7 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
-#SPLASH
+# SPLASH
 SPLASH_URL = 'http://127.0.0.1:8050'
 DOWNLOADER_MIDDLEWARES = {
     'scrapy_splash.SplashCookiesMiddleware': 723,
@@ -25,11 +25,11 @@ SPIDER_MIDDLEWARES = {
     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
 }
 
-#Download
+# Download
 DOWNLOAD_TIMEOUT = 10
 
-#MYSQL
-DB_SERVER = 'pymysql'            # For detail, please see twisted doc
+# MYSQL
+DB_SERVER = 'pymysql'  # For detail, please see twisted doc
 DB_CONNECT = {
     'db': 'test',
     'user': 'root',
@@ -43,21 +43,21 @@ DB_CONNECT = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 # run: items go through from lower valued to higher valued classes
 ITEM_PIPELINES = {
-   'crawler.pipelines.store.MysqlPipeline': 300,
+    'crawler.pipelines.store.MysqlPipeline': 300,
 }
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
-  'Accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36'
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+    'Accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36'
 }
 
-#CACHE
-HTTPCACHE_ENABLED=True
-HTTPCACHE_EXPIRATION_SECS=0
-HTTPCACHE_DIR='/Users/jason-geng/Code/python/crawler/httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES=[]
-HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
-# HTTPCACHE_POLICY='scrapy.extensions.httpcache.DummyPolicy' #仅开发使用
-HTTPCACHE_POLICY='scrapy.extensions.httpcache.RFC2616Policy' #生产环境使用
-
+# CACHE
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'cache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'  # 仅开发使用
+# HTTPCACHE_POLICY='scrapy.extensions.httpcache.RFC2616Policy' #生产环境使用
---
+++ crawler/spiders/search.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import time
+import scrapy
+
+
+class SearchSpider(scrapy.Spider):
+    name = 'search'
+    allowed_domains = []
+    start_urls = []
+
+    site_url = ""
+
+    match_words = [""]  # 要匹配的关键词
+    filter_words = [""]  # 要过滤的关键词
+
+    filename = "test_%s.html" % (time.strftime("%Y%m%d%H%M", time.localtime()))
+
+    def start_requests(self):
+
+        with open(self.filename, "w") as f:
+            f.write("<meta charset='UTF-8'>\n")
+
+        for i in range(1, 786):
+            url = "%sthread.php?fid=26&page=%d" % (self.site_url, i)
+            yield scrapy.Request(url=url,
+                                 callback=self.parse)
+
+    def parse(self, response):
+
+        xpath = "/html/body/div[@class='wrap']/div[@class='main-wrap']/div[@id='main']/div[@id='pw_content']/div[@id='sidebar']/div[@class='content_thread cc']/div[@class='content_ie']/div[@class='pd15'][2]/div[@id='ajaxtable']/div[@class='z threadCommon']/table/tbody[@id='threadlist']/tr[@class='tr3']/td[@class='subject']"
+
+        subject_sections = response.xpath(xpath)
+
+        with open(self.filename, "a") as f:
+            for section in subject_sections:
+
+                texts = section.css("a::text").extract()
+                subject = texts[0].encode("utf-8")
+                is_continue = True
+
+                # filter
+                for word in self.filter_words:
+                    if subject.find(word) >= 0:
+                        is_continue = False
+                        break
+
+                if not is_continue:
+                    continue
+
+                # match
+                for word in self.match_words:
+                    if subject.find(word) >= 0:
+                        links = section.css("a::attr(href)").extract()
+                        name = texts[0].encode("utf-8")
+                        link = str(self.site_url + links[0])
+                        res = "<a href='%s'>%s</a>" % (link, name)
+                        f.write(res + "<br>\n")
+                        break
