009275c8920f93af1bc610ed4214bca7fdb27336
gengjie02 <gengjie02@meituan.com>
2020-04-10 01:54:04 +0800

    add batch crawling logic

--- crawler/spiders/lianjia.py
+++ crawler/spiders/lianjia.py
@@ -13,7 +13,12 @@ from scrapy_splash import SplashRequest
 # 闵行七宝
 # html_url = "https://sh.lianjia.com/chengjiao/qibao/pg{0}a2a3a4/"
 
-area = "xinzhuang5"
+area_dict = {
+    # "chunshen": 44,
+    "qibao": 42,
+    "jinganxincheng": 36
+    # "xinzhuang5": 66
+}
 
 class SearchSpider(scrapy.Spider):
     name = "lianjia"
@@ -22,22 +27,27 @@ class SearchSpider(scrapy.Spider):
     start_urls = [
     ]
     meta = {}
-
-    html_url = "https://sh.lianjia.com/chengjiao/" + area + "/pg{0}a2a3a4/"
     
     html_headers = {
         "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4"
     }
 
-    filename = "lianjia_%s_%s.csv" % (area, time.strftime("%Y%m%d%H%M", time.localtime()))
-
+    
     def start_requests(self):
-        for i in range(1, 63):
-            url = self.html_url.format(i)
-            time.sleep(1)
-            # print(url)
-            yield scrapy.Request(url=url,
-                                 callback=self.parse)
+        for (k,v) in area_dict.items():
+            area = k
+            page_num = v
+            filename = "lianjia_%s_%s.csv" % (area, time.strftime("%Y%m%d%H%M", time.localtime()))
+            html_url = "https://sh.lianjia.com/chengjiao/" + area + "/pg{0}a2a3a4/"
+            for i in range(1, page_num):
+                url = html_url.format(i)
+                time.sleep(1)
+                # print(url)
+                yield scrapy.Request(url=url,
+                                     meta={'filename':filename},
+                                     callback=self.parse)
+                pass
+        
 
     def parse(self, response):
         path = "/html/body/div[@class='content']/div[@class='leftContent']/ul[@class='listContent']/li/div[@class='info']"
@@ -53,7 +63,7 @@ class SearchSpider(scrapy.Spider):
         house_info_path = "div[@class='address']/div[@class='houseInfo']/text()"
 
         sections = response.xpath(path)
-        with open(self.filename, "a") as f:
+        with open(response.meta['filename'], "a") as f:
             for item in sections:
                 name = item.xpath(name_path).extract()[0].encode("utf-8")
                 name_arr = name.split(" ")
